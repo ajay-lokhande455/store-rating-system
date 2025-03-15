4a996c4fd323aea9b4a202657067372ffbfe73b6
Ajay Lokhande <lokhandeajay455@gmail.com>
2025-03-15 19:34:48 +0530

    updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,15 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: false, 
+});
 
-sequelize
-  .authenticate()
-  .then(() => console.log('MySQL Connected Successfully'))
-  .catch((err) => console.error('Database Connection Error:', err));
+sequelize.authenticate()
+  .then(() => console.log("Database connected successfully."))
+  .catch(err => console.error("Error connecting to database:", err));
 
 module.exports = sequelize;
